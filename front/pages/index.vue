8e9fcb11c02b9b089942d4122cbf8666974eab02
Wathis <delaunaymathis@yahoo.fr>
2020-11-02 14:33:47 +0100

    routing

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        front
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <NuxtLink to="/tv-shows">Home page</NuxtLink>
+    <NuxtLink to="/tv-shows/1">Show Details</NuxtLink>
   </div>
 </template>
 
